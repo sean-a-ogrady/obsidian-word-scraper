ac1cdccac75a354ab7a62d6df1ca0606248d01be
Sean O'Grady <ogrady.sean.a@gmail.com>
2023-09-25 13:47:46 -0400

    Update UI, Capture words to file logic

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, addIcon } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, addIcon, TFile } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -12,22 +12,30 @@ const DEFAULT_SETTINGS: WordScraperSettings = {
 
 export default class WordScraperPlugin extends Plugin {
 	settings: WordScraperSettings;
+	private wordList: string[] = [];
+	private dailyMdFile: TFile | null = null;
+	private lastKnownDate: string = new Date().toISOString().slice(0, 10);
+	private statusBar: HTMLElement;
 
 	async onload() {
 		await this.loadSettings();
 
+		this.registerInterval(window.setInterval(this.checkDateAndReset.bind(this), 60 * 1000));  // Check every minute
+
+		this.registerCodeMirror((cm: CodeMirror.Editor) => {
+			cm.on("change", this.handleChange.bind(this));
+		});
+
 		// This creates an icon in the left ribbon.
-		const wordScraperIcon = addIcon('word-scraper-icon', `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="14 2 18 6 7 17 3 17 3 13 14 2"></polygon><line x1="3" y1="22" x2="21" y2="22"></line></svg>`);
-		const ribbonIconEl = this.addRibbonIcon('word-scraper-icon', 'Word Scraper', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('pencil', 'Word Scraper', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('Word Scraper Clicked!');
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		ribbonIconEl.addClass('word-scraper-ribbon');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.statusBar = this.addStatusBarItem();
+		this.statusBar.setText(`${this.wordList.length} words today`);
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
@@ -35,34 +43,6 @@ export default class WordScraperPlugin extends Plugin {
 			name: 'Open sample modal (simple)',
 			callback: () => {
 				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
 			}
 		});
 
@@ -80,7 +60,55 @@ export default class WordScraperPlugin extends Plugin {
 	}
 
 	onunload() {
+		this.wordList = [];
+		this.dailyMdFile = null;
+	}
 
+	private async handleChange(cm: CodeMirror.Editor, change: any): Promise<void> {
+		const newWords = change.text.join(" ").match(/\b\w+\b/g);
+		if (newWords) {
+			this.wordList.push(...newWords);
+			this.statusBar.setText(`${this.wordList.length} words today`);
+		}
+		await this.updateDailyMdFile();
+	}
+
+	private async updateDailyMdFile(): Promise<void> {
+		try {
+			const vault = this.app.vault;
+			const today = new Date().toISOString().slice(0, 10);
+			const fileName = `WordCloud-${today}.md`;
+
+			if (!this.dailyMdFile) {
+				this.dailyMdFile = await vault.getAbstractFileByPath(fileName) as TFile;
+			}
+
+			if (!this.dailyMdFile) {
+				this.dailyMdFile = await vault.create(fileName, '');
+			}
+
+			const content = [
+				'```wordcloud',
+				'source: file',
+				'```',
+				'',
+				...this.wordList
+			].join('\n');
+
+			await vault.modify(this.dailyMdFile, content);
+		} catch (error) {
+			console.error("An error occurred while updating the daily Markdown file:", error);
+		}
+	}
+
+	private async checkDateAndReset(): Promise<void> {
+		const currentDate = new Date().toISOString().slice(0, 10);
+		if (currentDate !== this.lastKnownDate) {
+			this.wordList = [];
+			this.lastKnownDate = currentDate;
+			this.dailyMdFile = null;  // Reset the daily file as well
+			await this.updateDailyMdFile();  // Create a new file for the new day
+		}
 	}
 
 	async loadSettings() {
@@ -98,12 +126,12 @@ class SampleModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.setText('Woah!');
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
@@ -117,7 +145,7 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
